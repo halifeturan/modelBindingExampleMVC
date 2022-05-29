cfe7302de61b26d4a1703773f17959cfcf0cd375
halife <halife0606@gmail.com>
2022-05-29 15:18:13 +0300

    Proje dosyası ekle.

---
+++ modelBindingExampleMvc/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modelBindingExampleMvc.Models.Context;
+using modelBindingExampleMvc.Models.Entities;
+using System.Linq;
+
+namespace modelBindingExampleMvc.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentsContext db = new StudentsContext();
+        public IActionResult Index()
+        {
+            var students = db.Students.ToList();
+            return View(students);
+        }
+
+
+
+        public IActionResult StudentCreate()
+        {
+            var students = db.Students.OrderBy(x => x.id).ToList();
+            return View(students);
+        }
+        [HttpPost]
+        public IActionResult StudentCreate(Student student)
+        {
+            if (student!=null)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+            
+            return View(db.Students.OrderBy(x=>x.id).ToList());
+        }
+
+    }
+}
